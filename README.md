4b0dc6f2e8b52fe496c65921e0fb6cc1745fb422
bruneltz <bruno.eltz@hotmail.com>
2025-08-25 13:43:10 +1000

    added endpoints

---
+++ README.md
@@ -0,0 +1,15 @@
+# NestJS
+
+# **Opinionated**
+# MVC
+# Decorators are TypeScript annotations (starting with @)
+
+## Setup
+
+- nest new <name>
+- nest g module <name> 
+- nest g service <name> 
+- nest g controller <name> 
+
+### Controllers
+- The order of endpoints matters. More specific routes should be defined before less specific ones.
--- lesson01/src/users/users.controller.ts
+++ lesson01/src/users/users.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    //The order of endpoints matters. More specific routes should be defined before less specific ones.
+
+    @Get() // GET /users
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN'): string {
+        return 'This action returns all users with role: ' + role;
+    }
+
+    @Get(':id') // GET /users/1
+    findOne(@Param('id') id: string ): string {
+        return 'This action returns a user by id: ' + id;
+    }
+
+    @Post() // POST /users
+    create(@Body() users: {}) {
+        return users
+    }
+
+    @Patch(':id') // PUT /users/1
+    update(@Param('id') id: string, @Body() userUpdate: {}) {
+        return { id, ...userUpdate };
+    }
+
+    @Delete(':id') // DELETE /users/1
+    delete(@Param('id') id: string ): string {
+        return 'Deleted id: ' + id;
+    }
+
+}
